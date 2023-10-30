23ca544999e83738caeaa34812aac6b9e4339a89
liangjinyong <liangjy881@gmail.com>
2023-10-30 19:06:37 +0900

    constants and variables

---
+++ ch03_learning_var_const_string_operator.playground/Contents.swift
@@ -0,0 +1,82 @@
+import UIKit
+
+// Chapter 3: Learning about Variables, Constants, Strings,  and Operators
+
+// * Constants and variables
+// ** Defining constants and variables
+// Constants
+
+/*
+let freezingTemperatureOfWaterCelsius = 0
+let speedOfLightKmSec = 300000
+ */
+
+// Variables
+/*
+var currentTemperature = 22
+var currentSpeed = 55
+ */
+
+// declare multiple constants or variables in a single line by separating them with a comma:
+// Constants
+let freezingTemperatureOfWaterCelsius = 0, speedOfLightKmSec = 300000
+
+// Variables
+var currentTemperature = 22, currentSpeed = 55
+
+// speedOfLightKmSec = 299999
+
+
+// ** Type safety
+var integerVar = 10
+// integerVar = "My String"
+
+// ** Type inference
+var myInt = 1
+
+// var x = 3.14 // Double type
+var y = "Hello" // String type
+var z = true // Boolean type
+
+// ** Explicit types
+// var x: Float = 3.14
+// var x: Float = "My str"
+// var x // Type annotation missing in pattern
+var x: Int // explicitly defind a variable a type
+
+
+// * Numeric types
+// ** Integer types
+
+// ** Floating-point and Double values
+
+// * The Boolean type
+
+// * The String type
+
+// * Tuples
+
+// * Enumerations
+
+// * Opertators
+// ** The assignment operator
+
+// ** Comparison operators
+
+// ** Arithmetic operators
+
+// ** The remainder operator
+
+// ** Compound assignment operators
+
+// ** The closed range operator
+
+// ** The half-open range operator
+
+// ** The ternary conditional operator
+
+// ** The logical NOT operator
+
+// ** The logical AND operator
+
+// ** The logical OR operator
