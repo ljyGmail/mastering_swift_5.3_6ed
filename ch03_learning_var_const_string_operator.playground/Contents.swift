7e04231f978bfaf4927a8cffbaab3e517dc3b915
liangjinyong <liangjy881@gmail.com>
2023-10-31 12:33:43 +0900

    tuples and enumerations

--- ch03_learning_var_const_string_operator.playground/Contents.swift
+++ ch03_learning_var_const_string_operator.playground/Contents.swift
@@ -248,8 +248,152 @@ path.first
 var length = path.count
 
 // * Tuples
+// define a tuple:
+// var team = ("Boston", "Red Sox", 97, 65, 59.9)
+
+// decompose a tuple into a set of variables
+/*
+var (city, name, wins, losses, percent) = team
+city
+name
+wins
+losses
+percent
+ */
+
+// the values of a tuple can also be retrieved by specifying the location of the value
+/*
+var city = team.0
+var name = team.1
+var wins = team.2
+var losses = team.3
+var percent = team.4
+city
+name
+wins
+losses
+percent
+ */
+
+// named tuples
+var team = (city:"Boston", name:"Red Sox", wins:97, losses:65, percent:59.9)
+team.city
+team.name
+team.wins
+team.losses
+team.percent
 
 // * Enumerations
+/*
+enum Planets {
+    case mercury
+    case venus
+    case earth
+    case mars
+    case Jupiter
+    case Saturn
+    case Uranus
+    case neptune
+}
+ */
+
+// a shorter version
+enum Planets {
+    case mecury, venus, earth, mars, jupiter
+    case saturn, uranus, neptune
+}
+
+// use the Planets enumeration like this:
+var planetWeLiveOn = Planets.earth
+var furthestPlanet = Planets.neptune
+
+// Once the variable type is inferred, a new value can be assigned without the Planet prefix
+planetWeLiveOn = .mars
+
+// Compare enumeration values
+
+// Using the traditional == operator
+if planetWeLiveOn == .earth {
+    print("Earth it is")
+}
+
+// Using the switch statement
+switch planetWeLiveOn {
+case .mecury:
+    print("We live on Mercury, it is very hot!")
+case .venus:
+    print("We live on Venus, it is very hot!")
+case .earth:
+    print("We live on Earth, just right")
+case .mars:
+    print("We live on Mars, a little cold")
+default:
+    print("Where do we live?")
+}
+
+// Define an enumeration with string values
+enum Devices: String {
+    case MusicPlayer = "iPod"
+    case Phone = "iPhone"
+    case Tablet = "iPad"
+}
+
+print("We are using an \(Devices.Tablet.rawValue)")
+
+// assign numbers to the members
+enum Planets2: Int {
+    case mercury = 1
+    case venus
+    case earth
+    case mars
+    case jupiter
+    case saturn
+    case uranus
+    case neptune
+}
+
+print("Earth is planet number \(Planets2.earth.rawValue)")
+
+// enumertions can also have associated values
+enum Product {
+    case Book(Double, Int, Int)
+    case Puzzle(Double, Int)
+}
+
+var masterSwift = Product.Book(49.99, 2017, 310)
+var worldPuzzle = Product.Puzzle(9.99, 200)
+
+switch masterSwift {
+case .Book(let price, let year, let pages):
+    print("Mastering Swift was published in \(year) for the price of \(price) and has \(pages) pages")
+case .Puzzle(let price, let pieces):
+    print("Mastering Swift is a puzzle with \(pieces)  and sells for \(price)")
+}
+
+switch worldPuzzle {
+case .Book(let price, let year, let pages):
+    print("World Puzzle was  published in \(year) for the price of \(price) and has \(pages) pages")
+case .Puzzle(let price, let pieces):
+    print("World Puzzle is a puzzle with \(pieces) and sells for \(price)")
+}
+
+// conformance of the Comparable protocol
+enum Grades: Comparable {
+    case f
+    case d
+    case c
+    case b
+    case a
+}
+
+let acceptableGrade = Grades.c
+let testOneGrade = Grades.b
+
+if testOneGrade < acceptableGrade {
+    print("Grade is unacceptable")
+} else {
+    print("Grade is acceptable")
+}
 
 // * Opertators
 // ** The assignment operator
